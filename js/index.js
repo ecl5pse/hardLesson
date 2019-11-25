23c8f3798f53e26d31d1857dccda8332fb5124fb
ecl5pse <fantamas20@gmail.com>
2019-11-25 20:42:43 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,94 @@
-'use strict'
+'use strict';
+
+
+/*
+const firstMap =  new Map();
+
+
+const  objKey = {
+    name: "Name",
+};
+
+
+firstMap.set("Dima", 2);
+firstMap.set(objKey , "value");
+firstMap.set(objKey , "value");
+
+
+
+const  secondMap =  new Map(
+
+    [
+        ["Dima" , 2]
+        ["Dima" , 25]
+        ["Dima" , 4]
+
+    ]
+)*/
+
+/*
+const arr = ["Flkfdjgd",[4,54,4,656,876],"hfdukhgfdjhgfdiu","fgoidfugfdgdhfcxv"];
+const [first,[ , second], third , ...rest] = arr;
+
+
+const obj = {
+    name:"Name",
+    surname:"Surname",
+    car:{
+        seatsCount: 5,
+        engine :{
+            volume:2.0,
+            voice: 'Wromm',
+        }
+    },
+};
+
+
+const {car: {engine :{volume: userEngineVolume } } } = obj;
+
+function getUserFullName  (user) {
+
+    return `${user.name} ${user.surname}`;
+
+}
+
+function getUserFullName2(user) {
+
+    const {name , surname}= user;
+    return`${name} ${surname}`;
+
+}
+
+function sum() {
+
+    let sum = 0;
+
+    for (let value of arguments){
+        sum+=value;
+    }
+    return sub;
+
+}
+
+const arrowSub = (...rest) =>{
+
+
+    let sum = 0;
+
+    console
+}*/
+
+/*
+const arr1 = [1,1,2,1,13,1,6847,6];
+const arr2 =["test1","test5","hgfh"];
+
+
+
+*/
+
+
+const obj1 = {
+    name: "Object1",
+    test:"test",
+}
+    
